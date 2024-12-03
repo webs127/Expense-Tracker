8b06c073abb8d94498e5c1a25e40090fa8f94c15
webs127 <davidshittu20@gmail.com>
2024-12-03 16:50:50 +0100

    Image manager
    Manages Image urls

---
+++ lib/core/manager/image_manager.dart
@@ -0,0 +1,7 @@
+class ImageManager {
+  static const String applogo = "asset/images/svg/app_logo.svg";
+  static const String onboarding1 = "asset/images/svg/onboarding 1.svg";
+  static const String onboarding2 = "asset/images/svg/onboarding 2.svg";
+  static const String onboarding3 = "asset/images/svg/onboarding 3.svg";
+  static const String applogohoriz = "asset/images/svg/horizontal logo.svg";
+}
