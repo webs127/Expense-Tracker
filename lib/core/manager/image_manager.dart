b4100fbf5ed542280fe61b282c41e46e177c406c
webs127 <davidshittu20@gmail.com>
2024-12-06 22:46:37 +0100

    New image urls

--- lib/core/manager/image_manager.dart
+++ lib/core/manager/image_manager.dart
@@ -4,4 +4,12 @@ class ImageManager {
   static const String onboarding2 = "asset/images/svg/onboarding 2.svg";
   static const String onboarding3 = "asset/images/svg/onboarding 3.svg";
   static const String applogohoriz = "asset/images/svg/horizontal logo.svg";
+  static const String home = "asset/images/svg/home-grey.svg";
+  static const String savings = "asset/images/svg/task.svg";
+  static const String reminder = "asset/images/svg/setting.svg";
+  static const String notification = "asset/images/svg/icon.svg";
+  static const String wallet = "asset/images/svg/wallet.svg";
+  static const String bike = "asset/images/svg/bike 1.svg";
+  static const String shoppingBag = "asset/images/svg/shopping-bags 1.svg";
+  static const String cashback = "asset/images/svg/cashback 1.svg";
 }
