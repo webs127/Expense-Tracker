0a07dbc37c36e2080be25a7df42f9a0c32fca49c
webs127 <davidshittu20@gmail.com>
2024-12-06 23:25:42 +0100

    Auth wrapper view implementation

---
+++ lib/ui/auth/auth.dart
@@ -0,0 +1,11 @@
+import 'package:expense_tracker/ui/landing/landing.dart';
+import 'package:flutter/material.dart';
+
+class AuthenticationScreen extends StatelessWidget {
+  const AuthenticationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const LandingScreen();
+  }
+}
