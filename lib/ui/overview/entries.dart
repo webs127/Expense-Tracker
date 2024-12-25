3b69cb9ca5b3cdf7b59c7b0ef4e469a026307245
webs127 <davidshittu20@gmail.com>
2024-12-25 12:16:12 +0100

    Screen for getting full list of entries data from the database

--- lib/ui/overview/entries.dart
+++ lib/ui/overview/entries.dart
@@ -1,7 +1,10 @@
 import 'package:expense_tracker/core/manager/color_manager.dart';
 import 'package:expense_tracker/core/manager/textstyle_manager.dart';
+import 'package:expense_tracker/core/models/expense_model.dart';
+import 'package:expense_tracker/ui/overview/controller/expense_controller.dart';
 import 'package:expense_tracker/widgets/shared/entry_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class EntriesScreen extends StatefulWidget {
   const EntriesScreen({super.key});
@@ -33,22 +36,47 @@ class _EntriesScreenState extends State<EntriesScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 24, top: 24),
-                child: Text(
-                  "Latest Entries",
-                  style: AppTextStyles.custom(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                      color: ColorManager.dark),
-                ),
-              ),
+              // Padding(
+              //   padding: const EdgeInsets.only(left: 24, top: 24),
+              //   child: Text(
+              //     "Latest Entries",
+              //     style: AppTextStyles.custom(
+              //         fontSize: 16,
+              //         fontWeight: FontWeight.w600,
+              //         color: ColorManager.dark),
+              //   ),
+              // ),
               const SizedBox(height: 20),
-
-              Column(
-                children: List.generate(50, (i) => const EntryTile(padding: 24)),
-              ),
-              //const SizedBox(height: 20),
+              Consumer<ExpenseController>(builder: (context, expense, __) {
+                return FutureBuilder<List<ExpenseModel>>(
+                    future: expense.getExpenses(),
+                    builder: (context, snapshot) {
+                      {
+                        if (snapshot.connectionState ==
+                            ConnectionState.waiting) {
+                          return const Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        } else {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 14),
+                            child: Column(
+                              children: List.generate(
+                                  snapshot.data!.length,
+                                  (index) => EntryTile(
+                                        padding: 10,
+                                        title: snapshot.data![index].name,
+                                        amount: snapshot.data![index].amount,
+                                        paymentMethod:
+                                            snapshot.data![index].paymentMethod,
+                                        date: snapshot.data![index].date,
+                                      )),
+                            ),
+                          );
+                        }
+                      }
+                    });
+              })
             ],
           ),
         ),
