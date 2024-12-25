41d70e0d9415590724198d9ee436823fc91887e4
webs127 <davidshittu20@gmail.com>
2024-12-25 11:53:03 +0100

    Integrated Database and Notification services for usage

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:expense_tracker/app/app.dart';
+import 'package:expense_tracker/services/notification_manager.dart';
+import 'package:expense_tracker/services/service.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DatabaseHelper().database;
+  await NotificationManager.initializeNotifications();
   runApp(MyApp());
 }
-
-
